d7464655eebf7963b40111aa540240f0aea838f2
faazizpro <fahadafs11@gmail.com>
2023-11-30 17:25:42 +0600

    Text Editor 1/3 part done

---
+++ JS/utilities.js
@@ -0,0 +1,36 @@
+document.getElementById('bold').addEventListener('click', function(){
+    document.getElementById('text-area').style.fontWeight = 'bold';
+});
+document.getElementById('italic').addEventListener('click', function(){
+    document.getElementById('text-area').style.fontStyle = 'italic';
+});
+document.getElementById('underline').addEventListener('click', function(){
+    document.getElementById('text-area').style.textDecoration = 'underline';
+});
+document.getElementById('left').addEventListener('click', function(){
+    document.getElementById('text-area').style.textAlign = 'left';
+});
+document.getElementById('center').addEventListener('click', function(){
+    document.getElementById('text-area').style.textAlign = 'center';
+});
+document.getElementById('right').addEventListener('click', function(){
+    document.getElementById('text-area').style.textAlign = 'right';
+});
+document.getElementById('lowercase').addEventListener('click', function(){
+    document.getElementById('text-area').style.textTransform = 'lowercase';
+});
+document.getElementById('uppercase').addEventListener('click', function(){
+    document.getElementById('text-area').style.textTransform = 'uppercase';
+});
+document.getElementById('capitalize').addEventListener('click', function(){
+    document.getElementById('text-area').style.textTransform = 'capitalize';
+});
+
+
+document.getElementById('clear').addEventListener('click', function(){
+    document.getElementById('text-area').style.fontWeight = 'normal';
+    document.getElementById('text-area').style.fontStyle = 'normal';
+    document.getElementById('text-area').style.textDecoration = 'none';
+    document.getElementById('text-area').style.textAlign = 'left';
+    document.getElementById('text-area').style.textTransform = 'lowercase';
+})
